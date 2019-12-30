75fd7d469517b5878b176a67786cb8fb661820ce
Marcos Laydner <marcoslaydner123@gmail.com>
2019-12-30 17:31:57 -0300

    normalized values on the dataset and used regression imputation for F20 instead of mean imputation. Slight improvements on acc are being achieved

--- src/feature_selection_test.py
+++ src/feature_selection_test.py
@@ -1,22 +1,27 @@
 import pandas as pd
 from sklearn import svm
-from sklearn.model_selection import StratifiedKFold, cross_val_score
-from sklearn.feature_selection import RFECV, SelectFromModel
+from sklearn.feature_selection import chi2, SelectPercentile
+from sklearn.model_selection import cross_val_score
 
 data = pd.read_csv("../resources/CE802_Ass_2019_Data - Adjusted.csv")
 feature_list = ['F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','F13','F14','F15','F16','F17','F18','F19','F20']
 
 X = data.loc[:, feature_list]
 y = data.Class
+clf = svm.SVC(kernel='rbf', C=1)
 
-print(cross_val_score(svm.SVC(kernel='rbf', C=1).fit(X, y), X, y, cv=10).mean())
+print(cross_val_score(clf.fit(X, y), X, y, cv=10).mean())
 
 # rfecv = RFECV(estimator=svm.SVC(kernel='linear', C=1), step=1, cv=StratifiedKFold(10), scoring='accuracy')
 # rfecv.fit(X, y)
 #
 # print(rfecv.grid_scores_)
 
-selector = SelectFromModel(estimator=svm.SVC(kernel='linear', C=1)).fit(X, y)
+# selector = SelectFromModel(estimator=svm.SVC(kernel='linear', C=1)).fit(X, y)
+#
+#
+# print(cross_val_score(svm.SVC(kernel='rbf', C=1).fit(selector.transform(X), y), selector.transform(X), y, cv=10).mean())
 
-
-print(cross_val_score(svm.SVC(kernel='rbf', C=1).fit(selector.transform(X), y), selector.transform(X), y, cv=10).mean())
+X_new = SelectPercentile(chi2, percentile=10).fit_transform(X, y)
+clf1 = svm.SVC(kernel='rbf', C=1)
+print((cross_val_score(clf1.fit(X_new, y), X_new, y, cv=10)).mean())
--- src/models.py
+++ src/models.py
@@ -1,10 +1,14 @@
 import copy
 
+import numpy as np
 import pandas as pd
 from sklearn import svm
 from sklearn import tree
+from sklearn.impute import SimpleImputer
+from sklearn.impute._iterative import IterativeImputer
 from sklearn.model_selection import cross_val_score
 from sklearn.naive_bayes import GaussianNB
+from sklearn import preprocessing
 
 
 def train_model(data, features, classifier):
@@ -19,7 +23,6 @@ def train_model(data, features, classifier):
 def feature_selection(score, classifier):
 
     useful_features = copy.deepcopy(feature_list)
-    print(useful_features)
     max_score = score
 
     for feature in feature_list:
@@ -35,21 +38,36 @@ def feature_selection(score, classifier):
     return useful_features
 
 
-data = pd.read_csv("../resources/CE802_Ass_2019_Data - Adjusted.csv")
+def model_comparison(dataset):
+    dt_score = train_model(dataset, feature_list, tree.DecisionTreeClassifier(random_state=0))
+    svm_score = train_model(dataset, feature_list, svm.SVC(kernel='rbf', C=1))
+    nb_score = train_model(dataset, feature_list, GaussianNB())
+
+    print(dt_score, svm_score, nb_score)
+
+    print("DT: ")
+    feature_selection(dt_score, tree.DecisionTreeClassifier(random_state=0))
+    print("SVM: ")
+    feature_selection(svm_score, svm.SVC(kernel='rbf', C=1))
+    print("NB: ")
+    feature_selection(nb_score, GaussianNB())
+
+
+data = pd.read_csv("../resources/CE802_Ass_2019_Data.csv")
 feature_list = ['F1','F2','F3','F4','F5','F6','F7','F8','F9','F10','F11','F12','F13','F14','F15','F16','F17','F18','F19','F20']
 
-dt_score = train_model(data, feature_list, tree.DecisionTreeClassifier(random_state=0))
-svm_score = train_model(data, feature_list, svm.SVC(kernel='rbf', C=1))
-nb_score = train_model(data, feature_list, GaussianNB())
+imputed_data = IterativeImputer(max_iter=10, random_state=0).fit_transform(data.values)
+data = pd.DataFrame(imputed_data, columns=data.columns, index=data.index)
+print(data.values)
 
-print(dt_score)
-print(svm_score)
-print(nb_score)
+normalized_values = preprocessing.MinMaxScaler().fit_transform(data.values)
+normalized_data = pd.DataFrame(normalized_values, columns=data.columns, index=data.index)
 
-print("DT: ")
-feature_selection(dt_score, tree.DecisionTreeClassifier(random_state=0))
-print("SVM: ")
-feature_selection(svm_score, svm.SVC(kernel='linear', C=1))
-print("NB: ")
-feature_selection(nb_score, svm.SVC(kernel='linear', C=1))
+print("Not Normalized")
+model_comparison(data)
+print("\nNormalized")
+model_comparison(normalized_data)
+
+
+
 
